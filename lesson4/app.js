4a0bf6cf02e1eba231af06eccfe89279aed8bed3
YangShaoQun <shaoquny@gmail.com>
2016-03-18 22:15:14 +0800

    lesson04 got

--- lesson4/app.js
+++ lesson4/app.js
@@ -1,6 +1,8 @@
 var eventproxy = require('eventproxy');
 var superagent = require('superagent');
 var cheerio = require('cheerio');
+// url 模块是 Node.js 标准库里面的
+// http://nodejs.org/api/url.html
 var url = require('url');
 
 var cnodeUrl = 'https://cnodejs.org/';
@@ -12,36 +14,66 @@ superagent.get(cnodeUrl)
     }
     var topicUrls = [];
     var $ = cheerio.load(res.text);
+    // 获取首页所有的链接
     $('#topic_list .topic_title').each(function (idx, element) {
       var $element = $(element);
+      // $element.attr('href') 本来的样子是 /topic/542acd7d5d28233425538b04
+      // 我们用 url.resolve 来自动推断出完整 url，变成
+      // https://cnodejs.org/topic/542acd7d5d28233425538b04 的形式
+      // 具体请看 http://nodejs.org/api/url.html#url_url_resolve_from_to 的示例
       var href = url.resolve(cnodeUrl, $element.attr('href'));
       topicUrls.push(href);
     });
 
-    var ep = new eventproxy();
+    console.log(topicUrls);
 
-    ep.after('topic_html', topicUrls.length, function (topics) {
-      topics = topics.map(function (topicPair) {
-        var topicUrl = topicPair[0];
-        var topicHtml = topicPair[1];
-        var $ = cheerio.load(topicHtml);
-        return ({
-          title: $('.topic_full_title').text().trim(),
-          href: topicUrl,
-          comment1: $('.reply_content').eq(0).text().trim(),
-        });
-      });
+    deep_crawler(topicUrls);
+});
 
-      console.log('final:');
-      console.log(topics);
-    });
+function deep_crawler(topicUrls) {
+  // 命令 ep 重复监听 topicUrls.length 次（在这里也就是 40 次） `topic_html` 事件再行动
+  var ep = new eventproxy();
+  var topic_list = [];
+  ep.after('topic_html', topicUrls.length, function (topics) {
+    // topics 是个数组，包含了 40 次 ep.emit('topic_html', pair) 中的那 40 个 pair
 
-    topicUrls.forEach(function (topicUrl) {
+    // 开始行动
+    topic_list = topics.map(function (topicPair) {
+      // 接下来都是 jquery 的用法了
+      var topicUrl = topicPair[0];
+      var topicHtml = topicPair[1];
+      var $ = cheerio.load(topicHtml);
       superagent.get(topicUrl)
-        .end(function (err, res) {
-          console.log('fetch ' + topicUrl + ' successful');
-          ep.emit('topic_html', [topicUrl, res.text]);
+        .end(function(err, res) {
+          ep.emit('first_comment_author', res.text);
         });
+      return ({
+        title: $('.topic_full_title').text().trim(),
+        href: topicUrl,
+        first_comment: $('.reply_content').eq(0).text().trim(),
+      });
     });
   });
+
+
+  ep.after('first_comment_author', topicUrls.length, function(authors) {
+    
+    for (var i = 0; i < authors.length; i++) {
+      var $ = cheerio.load(authors[i]);
+      topic_list[i].first_comment_author = $('.user_name').text();
+      topic_list[i].first_commemt_author_score = $('.big').text();
+    };
+
+    console.log('final:');
+    console.log(topic_list);
+  });
+
+  topicUrls.forEach(function (topicUrl) {
+    superagent.get(topicUrl)
+      .end(function (err, res) {
+        console.log('fetch ' + topicUrl + ' successful');
+        ep.emit('topic_html', [topicUrl, res.text]);
+      });
+    });
+}
 

6f042235a10e0aa8c9c74558b188526ca3cffaa4
YangShaoQun <shaoquny@gmail.com>
2016-03-19 15:19:54 +0800

    update lesson05

--- lesson5/app.js
+++ lesson5/app.js
@@ -1,4 +1,41 @@
 var async = require('async');
+var superagent = require('superagent');
+var cheerio = require('cheerio');
+// url 模块是 Node.js 标准库里面的
+// http://nodejs.org/api/url.html
+var url = require('url');
+
+var cnodeUrl = 'https://cnodejs.org/';
+
+superagent.get(cnodeUrl)
+  .end(function (err, res) {
+    if (err) {
+      return console.error(err);
+    }
+    var topicUrls = [];
+    var $ = cheerio.load(res.text);
+    // 获取首页所有的链接
+    $('#topic_list .topic_title').each(function (idx, element) {
+      var $element = $(element);
+      // $element.attr('href') 本来的样子是 /topic/542acd7d5d28233425538b04
+      // 我们用 url.resolve 来自动推断出完整 url，变成
+      // https://cnodejs.org/topic/542acd7d5d28233425538b04 的形式
+      // 具体请看 http://nodejs.org/api/url.html#url_url_resolve_from_to 的示例
+      var href = url.resolve(cnodeUrl, $element.attr('href'));
+      topicUrls.push(href);
+    });
+
+    console.log(topicUrls);
+
+    async.mapLimit(topicUrls, 5, 
+      function (url, callback) {
+        fetchUrl(url, callback);
+      }, 
+      function (err, result) {
+        console.log('final:');
+        console.log(result);
+      });
+});
 
 var concurrencyCount = 0;
 var fetchUrl = function (url, callback) {
@@ -6,19 +43,17 @@ var fetchUrl = function (url, callback) {
   concurrencyCount++;
   console.log('现在的并发数是', concurrencyCount, '，正在抓取的是', url, '，耗时' + delay + '毫秒');
   setTimeout(function () {
-    concurrencyCount--;
-    callback(null, url + ' html content');
+    superagent.get(url)
+      .end(function(err, res) {
+        var $ = cheerio.load(res.text);
+        var result = {
+          title: $('.topic_full_title').text().trim(),
+          href: url,
+          first_comment: $('.reply_content').eq(0).text().trim()
+        };
+        concurrencyCount--;
+        callback(null, result);
+      });
   }, delay);
 };
 
-var urls = [];
-for(var i = 0; i < 30; i++) {
-  urls.push('http://datasource_' + i);
-}
-
-async.mapLimit(urls, 5, function (url, callback) {
-  fetchUrl(url, callback);
-}, function (err, result) {
-  console.log('final:');
-  console.log(result);
-});
